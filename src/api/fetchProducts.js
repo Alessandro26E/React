30acf60f926d4ffbef2fee6a156c207e7563bead
Alessandro26E <161986879+Alessandro26E@users.noreply.github.com>
2026-01-24 20:14:36 -0300

    atualizando projeto

--- src/api/fetchProducts.js
+++
@@ -1,7 +0,0 @@
-const getProducts = async (query) => {
-    const resp = await fetch(`https://dummyjson.com/products/search?q=${query}`)
-    const data = await resp.json()
-    return  data.products
-};
-
-export default getProducts;
--- src/utils/formatNumber.js
+++
@@ -1,5 +0,0 @@
-function formatNumber(number) {
-   return number.toLocaleString('pt-br', {style: 'currency', currency: "BRL"})
-}
-
-export default formatNumber;
