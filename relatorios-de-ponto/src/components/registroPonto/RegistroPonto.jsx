77e27387d8dda87f04ccfe0558e437563cba9924
Alessandro26E <161986879+Alessandro26E@users.noreply.github.com>
2026-01-29 22:02:42 -0300

    novo projeto

--- relatorios-de-ponto/src/components/registroPonto/RegistroPonto.jsx
+++ relatorios-de-ponto/src/components/registroPonto/RegistroPonto.jsx
@@ -11,7 +11,7 @@ function RegistroPonto() {
     useEffect(() => {
         setSeconds(data.getSeconds())
     }, [seconds])
-    
+    console.log("Oi")
     return (
         <div className="registro-container">
             <div className='div-hora'>
